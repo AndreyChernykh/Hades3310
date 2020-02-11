d8bc2edda1a5def61bff68ab7b34468836180dcc
Andrei Chernykh <a.d.chernykh@gmail.com>
2020-02-10 21:37:48 -0800

    Removed LeanTween

--- Assets/Scripts/Map/YouAreHere.cs
+++ Assets/Scripts/Map/YouAreHere.cs
@@ -2,19 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class YouAreHere : MonoBehaviour {
-    public float transitionTime = 5;
-    public float blinkTime = 0.5f;
-    public Color color1;
-    public Color color2;
+    [SerializeField]
+    private float blinkTime = 0.5f;
+    [SerializeField]
+    private Color color1;
+    [SerializeField]
+    private Color color2;
 
-    List<Vector2> points = new List<Vector2>();
+    private float speed = 0.07f;
+    private List<Vector2> points = new List<Vector2>();
 
-    float timeBeforeNextBlink;
-    SpriteRenderer sprite;
+    private float timeBeforeNextBlink;
+    private SpriteRenderer sprite;
+
+    private Vector2 targetPoint;
+    private bool isArrived = false;
 
     private void Start() {
-        points.Add(new Vector2(25.1f, -20.9f));
+        sprite = GetComponent<SpriteRenderer>();
+
+        timeBeforeNextBlink = blinkTime;
+
+        SetupPoints();
+    }
+
+    private void SetupPoints() {
+        // Nothing ever easy with Nokia 3310
+        points.Add(new Vector2(25, -20.6f));
         points.Add(new Vector2(2.2f, -20.6f));
         points.Add(new Vector2(-9, -20.6f));
         points.Add(new Vector2(-19.6f, -20.6f));
@@ -30,15 +46,17 @@ public class YouAreHere : MonoBehaviour {
         points.Add(new Vector2(4.2f, -0.6f));
         points.Add(new Vector2(4.2f, 4.5f));
 
-        sprite = GetComponent<SpriteRenderer>();
-        timeBeforeNextBlink = blinkTime;
-        MoveToNextPoint();
-
         Vector2 initialPoint = points[Stats.currentRoom];
         transform.position = new Vector3(initialPoint.x, initialPoint.y, 0);
+        targetPoint = points[Stats.currentRoom + 1];
     }
 
-    void Update() {
+    private void Update() {
+        Blink();
+        MoveToNextPoint();
+    }
+
+    private void Blink() {
         if (timeBeforeNextBlink < 0) {
             sprite.color = sprite.color == color1 ? color2 : color1;
             timeBeforeNextBlink = blinkTime;
@@ -47,11 +65,18 @@ public class YouAreHere : MonoBehaviour {
         timeBeforeNextBlink -= Time.deltaTime;
     }
 
-    [ContextMenu("MoveToNextPoint")]
     public void MoveToNextPoint() {
-        LeanTween.move(gameObject, points[Stats.currentRoom + 1], transitionTime).setOnComplete(() => {
+        if (isArrived) {
+            return;
+        }
+
+        if (Vector2.Distance(transform.position, targetPoint) < 0.1f) {
+            isArrived = true;
             Stats.currentRoom += 1;
             LevelLoader.Instance.LoadNextLevel();
-        });
+        }
+        else {
+            transform.position = Vector2.MoveTowards(transform.position, targetPoint, speed);
+        }
     }
 }
--- Assets/TODO.md
+++ Assets/TODO.md
@@ -1,8 +1,4 @@
-﻿- Demon attack (shoot projectiles) 
-- Level generator 
-- Gods blessings: Zeus (random lightning), Dionysus (+restore life and +max life a little) Ares (+max power), Poseidon (+money), Aphrodite (+ max life)
-- Hades
-- Boss (Cerberus)
-- Shop
-- Initial hall
-- Music: intro, shop, gods blessings, regular, boss fight
+﻿- Check Update methods
+- FPS meter
+- Profiler
+- Modal dialog
